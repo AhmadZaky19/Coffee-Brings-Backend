a7a0216081db901a37028f6bf44b42c013a0a4a0
AhmadZaky19 <a.zaky32@gmail.com>
2021-11-25 00:50:14 +0700

    add authentication, isAdmin middleware

--- src/modules/product/productRoutes.js
+++ src/modules/product/productRoutes.js
@@ -3,34 +3,42 @@ const express = require("express");
 const Router = express.Router();
 
 const productController = require("./productController");
-// const middlewareAuth = require("../../middleware/auth");
+const middlewareAuth = require("../../middleware/auth");
 const middlewareRedis = require("../../middleware/redis");
 const middlewareUpload = require("../../middleware/uploadProduct");
 
 Router.get(
   "/",
+  middlewareAuth.authentication,
   middlewareRedis.getProductRedis,
   productController.getAllProduct
 );
 Router.get(
   "/:id",
+  middlewareAuth.authentication,
   middlewareRedis.getProductByIdRedis,
   productController.getProductById
 );
 Router.post(
   "/",
+  middlewareAuth.authentication,
+  middlewareAuth.isAdmin,
   middlewareRedis.clearProductRedis,
   middlewareUpload,
   productController.postProduct
 );
 Router.patch(
   "/:id",
+  middlewareAuth.authentication,
+  middlewareAuth.isAdmin,
   middlewareRedis.clearProductRedis,
   middlewareUpload,
   productController.updateProduct
 );
 Router.delete(
   "/:id",
+  middlewareAuth.authentication,
+  middlewareAuth.isAdmin,
   productController.deleteProduct,
   middlewareRedis.clearProductRedis
 );
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const express = require("express");
 const Router = express.Router();
 const userRoutes = require("../modules/user/userRoutes");
 const authRoutes = require("../modules/auth/authRoutes");
+const productRoutes = require("../modules/product/productRoutes");
 
 Router.use("/user", userRoutes);
 Router.use("/auth", authRoutes);
+Router.use("/product", productRoutes);
 
 module.exports = Router;
