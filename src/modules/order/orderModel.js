cce71d918ccb55b14e7fb3976daaab835b335523
Novianaa <noviana.rahmawati80@gmail.com>
2021-11-25 14:43:44 +0700

    Order

---
+++ src/modules/order/orderModel.js
@@ -0,0 +1,40 @@
+const connection = require("../../config/mysql")
+// const { getOrderByUserId } = require("./orderController")
+
+module.exports = {
+  postOrder: (data) =>
+    new Promise((resolve, reject) => {
+      connection.query("INSERT INTO order SET ?", data, (err, result) => {
+        if (!err) {
+          const newResult = {
+            id: result.insertId, ...data
+          }
+          resolve(newResult)
+        } else {
+          reject(new Error(`SQL: ${err.sqlMessage}`))
+        }
+      })
+    }),
+  getOrderByUserId: (idUser) =>
+    new Promise((resolve, reject) => {
+      connection.query(
+        `SELECT * FROM order WHERE idUser = '${idUser}'`, (error, result) => {
+          if (!error) {
+            resolve(result);
+          } else {
+            reject(new Error(`SQL: ${error.sqlMessage}`))
+          }
+        }
+      )
+    }),
+  deleteOrder: (id) =>
+    new Promise((resolve, reject) => {
+      connection.query("DELETE FROM order WHERE id=?", id, (error) => {
+        if (!error) {
+          resolve(id)
+        } else {
+          reject(new Error(`SQL : ${error.sqlMessage}`))
+        }
+      })
+    })
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,11 @@ const express = require("express");
 const Router = express.Router();
 const userRoutes = require("../modules/user/userRoutes");
 const authRoutes = require("../modules/auth/authRoutes")
+const orderRoutes = require("../modules/order/orderRoutes")
 
 Router.use("/user", userRoutes);
 Router.use("/auth", authRoutes)
+Router.use("/order", orderRoutes)
+
 
 module.exports = Router;
